6d2714ed828b6b9b45f96851044bc065f038d01e
Inquient <makoveyfedorich@gmail.com>
2018-03-16 19:38:11 +0300

    Добавлена некоторая визуализация

--- DataVisualization.py
+++ DataVisualization.py
@@ -28,10 +28,36 @@ std_ibu = numpy.sqrt(var_ibu)
 print("Значение медианы - {:.4f}; и Среднее значение{:.4f}".format(numpy.median(abv), numpy.mean(abv)))
 print("Значение медианы - {:.4f}; и Среднее значение{:.4f}".format(numpy.median(ibu), numpy.mean(ibu)))
 
-beers.hist(column='abv', edgecolor='white')
-pyplot.boxplot(abv, labels=["Содержание алкоголя, %"])
-pyplot.boxplot(ibu, labels=["Индекс горечи"])
+# beers.hist(column='abv', edgecolor='white')
+# pyplot.show()
+
+# pyplot.boxplot(abv, labels=["Содержание алкоголя, %"])
+# pyplot.show()
+#
+# pyplot.boxplot(ibu, labels=["Индекс горечи"])
+# pyplot.show()
+
+print("Первый квартиль для abv {} и ibu {}".format(numpy.percentile(abv, q=25), numpy.percentile(ibu, q=25)))
+print("Второй квартиль для abv {} и ibu {}".format(numpy.percentile(abv, q=50), numpy.percentile(ibu, q=50)))
+print("Третий квартиль для abv {} и ibu {}".format(numpy.percentile(abv, q=75), numpy.percentile(ibu, q=75)))
+
+# Визуализируем качественные данные
+
+style_series = beers['style']
+style_counts = style_series.value_counts() # Расчитывает количество повторов одного элемента в массиве
+style_counts[0:20].plot.barh(figsize=(10,8), color='#008367', edgecolor='gray')
 pyplot.show()
 
-print(numpy.percentile(abv, q=0), numpy.percentile(ibu, q=0))
+# Совместная визуализация различных данных
+
+beers_clean = beers.dropna() # Очищаем данные от потерянных значений
+abv = beers_clean['abv'].values
+ibu = beers_clean['ibu'].values
+
+pyplot.figure(figsize=(8,8))
+pyplot.scatter(abv, ibu, color='#3498db')
+pyplot.title('Scatter plot of alcohol-by-volume vs. IBU \n')
+pyplot.xlabel('abv')
+pyplot.ylabel('IBU')
+pyplot.show()
 
